123c1f08ebee913f0463e75aca8332850dce37c9
unknown <geo.mahmoudtaha@gmail.com>
2022-07-29 01:06:07 +0200

    remove contract

--- src/Contracts/WebsiteManagerContract.php
+++ src/Contracts/WebsiteManagerContract.php
@@ -13,18 +13,6 @@ interface WebsiteManagerContract
     public function handleRequest($route, $action);
 
     /**
-     * Render the website manager overview page.
-     */
-    public function renderOverview();
-
-    /**
-     * Render the website manager page settings (add/edit page form).
-     *
-     * @param PageContract $page
-     */
-    public function renderPageSettings(PageContract $page = null);
-
-    /**
      * Render the website manager menu settings (add/edit menu form).
      */
     public function renderMenuSettings();
